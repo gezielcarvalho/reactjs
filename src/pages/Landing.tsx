998bf6e6619da4222b62fa87db79c4450e73d572
Geziel Carvalho <geziel.natal@gmail.com>
2022-10-16 01:33:44 -0300

    Page link

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -16,7 +16,7 @@ const Landing = () => {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
+                            <Nav.Link href="/second">Second</Nav.Link>
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">
